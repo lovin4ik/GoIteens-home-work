549d3f82c98b3d99d8a449451b11c2f692f9bb85
lovin4ik <dianasmidt721@gmail.com>
2024-01-24 19:58:39 +0200

    addet portfolio webstudio

--- Js/modal.js
+++
@@ -1,6 +0,0 @@
-const modal = document.querySelector('.webstudio__modal');
-const btn = document.querySelector('.page04__hero__btn');
-
-let toggle = modal.classList.toggle('.off');
-
-btn.addEventListener('click', toggle);
